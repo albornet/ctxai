c7c3528cedd6b67c9bdd621c81c0bbece6368c88
albornet <alban.bornet@gmail.com>
2024-01-27 21:52:08 +0100

    added ports to wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,9 @@
 import logging
 from app import app
 
+HOST_ADDRESS = '0.0.0.0'
+PORT_NUMBER = 8984
+    
 
 def main():
     # Set logging level and format
@@ -10,7 +13,7 @@ def main():
     )
     
     # Run the app
-    app.run()
+    app.run(debug=False, host=HOST_ADDRESS, port=PORT_NUMBER)
 
 
 if __name__ == "__main__":
