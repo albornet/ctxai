fa22f984cec044e320f5a8b21bb709d4a23101fa
albornet <alban.bornet@gmail.com>
2024-02-02 17:57:00 +0100

    added typical prompt

--- wsgi.py
+++ wsgi.py
@@ -22,4 +22,19 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
+
+
+# Typical prompt
+{
+  "user_id": "1234",
+  "project_id": "5678",
+  "model_id": "pubmed-bert-sentence",
+  "raw_data_path": "data/raw_files/ctxai/intervention_similar_trials.xlsx",
+  "cluster_summarization_params":
+    {
+      "method": "gpt",
+      "n_representants": 20,
+      "gpt_system_prompt": "You are an expert in the fields of clinical trials and eligibility criteria. You express yourself succintly, i.e., less than 5 words per response.",
+      "gpt_user_prompt_intro": "I will show you a list of eligility criteria. They share some level of similarity. Your task is to create a single, short tag that effecively summarizes the list. Importantly, the tag should be very short and concise, i.e., under 5 words. You can use medical abbreviations and you should avoid focusing on details or outliers. Write only your answer, starting with 'Inclusion - ' or 'Exclusion - '. Summarize the criteria list as a whole, choosing either 'Inclusion' or 'Exclusion' for your tag, and not both. Here is the list of criteria (each one is on a new line):\n"
+    }
+}
