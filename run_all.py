9e00d8b97bd7ca73587b0a1ee25b266f3fb82fd9
albornet <alban.bornet@gmail.com>
2024-06-27 09:35:49 +0200

    run_all can be run in one command (useful for hpc)

--- run_all.py
+++ run_all.py
@@ -2,16 +2,24 @@ from src import config
 import json
 import subprocess
 import time
-from flask import Response
+import os
+import signal
+
 config.load_default_config()
 logger = config.CTxAILogger("INFO")
+
+
+def run_wsgi_server():
+    """ Function to start the WSGI server """
+    command = ["python", "wsgi.py"]
+    return subprocess.Popen(command)
 
 
 def run_curl_command(
     chosen_cond_ids: list[str],
     chosen_cond_lvl: int,
     chosen_itrv_lvl: int,
-) -> Response:
+):
     """ Query the clustering API with one set of experimental conditions
 
     Args:
@@ -20,7 +28,7 @@ def run_curl_command(
         chosen_itrv_lvl (int): intervention level from which labels are built
 
     Returns:
-        Response: response from the clustering API
+        response from the clustering API
     """
     query_dict = {
         "SCRIPT_MODE": True,
@@ -44,35 +52,46 @@ def run_curl_command(
 def main():
     """ Run the clustering pipeline for many different experimental conditions
     """
-    # Define your sequence of environments and data paths
-    tasks = [
-        # {"chosen_cond_ids": ["C01"], "chosen_cond_lvl": 2, "chosen_itrv_lvl": 1},
-        # {"chosen_cond_ids": ["C01"], "chosen_cond_lvl": 3, "chosen_itrv_lvl": 2},
-        # {"chosen_cond_ids": ["C01"], "chosen_cond_lvl": 4, "chosen_itrv_lvl": 3},
-        # {"chosen_cond_ids": ["C04"], "chosen_cond_lvl": 2, "chosen_itrv_lvl": 1},
-        # {"chosen_cond_ids": ["C04"], "chosen_cond_lvl": 3, "chosen_itrv_lvl": 2},
-        # {"chosen_cond_ids": ["C04"], "chosen_cond_lvl": 4, "chosen_itrv_lvl": 3},
-        # {"chosen_cond_ids": ["C14"], "chosen_cond_lvl": 2, "chosen_itrv_lvl": 1},
-        # {"chosen_cond_ids": ["C14"], "chosen_cond_lvl": 3, "chosen_itrv_lvl": 2},
-        # {"chosen_cond_ids": ["C14"], "chosen_cond_lvl": 4, "chosen_itrv_lvl": 3},
-        # {"chosen_cond_ids": ["C20"], "chosen_cond_lvl": 2, "chosen_itrv_lvl": 1},
-        # {"chosen_cond_ids": ["C20"], "chosen_cond_lvl": 3, "chosen_itrv_lvl": 2},
-        # {"chosen_cond_ids": ["C20"], "chosen_cond_lvl": 4, "chosen_itrv_lvl": 3},
-        {"chosen_cond_ids": ["C01", "C04", "C14", "C20"], "chosen_cond_lvl": 2, "chosen_itrv_lvl": 1},
-        {"chosen_cond_ids": ["C01", "C04", "C14", "C20"], "chosen_cond_lvl": 3, "chosen_itrv_lvl": 2},
-        {"chosen_cond_ids": ["C01", "C04", "C14", "C20"], "chosen_cond_lvl": 4, "chosen_itrv_lvl": 3},
-    ]
+    # Start the WSGI server
+    wsgi_process = run_wsgi_server()
+    
+    # Give the server some time to start
+    time.sleep(20)
 
-    for task in tasks:
-        logger.info(f"Sending curl request with {task}")
-        result = run_curl_command(**task)
-        if result.returncode == 0:
-            logger.info("Curl request sent and received successfully")
-        else:
-            logger.error(f"Curl request failed (code {result.returncode})")
-            logger.error(f"Detailed error: {result.stderr}")
-        time.sleep(10)  # delay to avoid overloading the server
+    try:
+        # Define your sequence of environments and data paths
+        tasks = [
+            {"chosen_cond_ids": ["C01"], "chosen_cond_lvl": 2, "chosen_itrv_lvl": 1},
+            {"chosen_cond_ids": ["C01"], "chosen_cond_lvl": 3, "chosen_itrv_lvl": 2},
+            {"chosen_cond_ids": ["C01"], "chosen_cond_lvl": 4, "chosen_itrv_lvl": 3},
+            {"chosen_cond_ids": ["C04"], "chosen_cond_lvl": 2, "chosen_itrv_lvl": 1},
+            {"chosen_cond_ids": ["C04"], "chosen_cond_lvl": 3, "chosen_itrv_lvl": 2},
+            {"chosen_cond_ids": ["C04"], "chosen_cond_lvl": 4, "chosen_itrv_lvl": 3},
+            {"chosen_cond_ids": ["C14"], "chosen_cond_lvl": 2, "chosen_itrv_lvl": 1},
+            {"chosen_cond_ids": ["C14"], "chosen_cond_lvl": 3, "chosen_itrv_lvl": 2},
+            {"chosen_cond_ids": ["C14"], "chosen_cond_lvl": 4, "chosen_itrv_lvl": 3},
+            {"chosen_cond_ids": ["C20"], "chosen_cond_lvl": 2, "chosen_itrv_lvl": 1},
+            {"chosen_cond_ids": ["C20"], "chosen_cond_lvl": 3, "chosen_itrv_lvl": 2},
+            {"chosen_cond_ids": ["C20"], "chosen_cond_lvl": 4, "chosen_itrv_lvl": 3},
+            {"chosen_cond_ids": ["C01", "C04", "C14", "C20"], "chosen_cond_lvl": 2, "chosen_itrv_lvl": 1},
+            {"chosen_cond_ids": ["C01", "C04", "C14", "C20"], "chosen_cond_lvl": 3, "chosen_itrv_lvl": 2},
+            {"chosen_cond_ids": ["C01", "C04", "C14", "C20"], "chosen_cond_lvl": 4, "chosen_itrv_lvl": 3},
+        ]
 
+        for task in tasks:
+            logger.info(f"Sending curl request with {task}")
+            result = run_curl_command(**task)
+            if result.returncode == 0:
+                logger.info("Curl request sent and received successfully")
+            else:
+                logger.error(f"Curl request failed (code {result.returncode})")
+                logger.error(f"Detailed error: {result.stderr}")
+            time.sleep(10)  # delay to avoid overloading the server
+    
+    finally:
+        # Terminate the WSGI server process
+        os.kill(wsgi_process.pid, signal.SIGTERM)
+        
 
 if __name__ == "__main__":
     main()
